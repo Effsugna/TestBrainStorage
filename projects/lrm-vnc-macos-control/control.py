fa650723bed62933a6477fa29885c8cd6b8e3a92
Effsugna <effsugna@gmail.com>
2025-07-16 17:13:27 +0930

    Add projects/lrm-vnc-macos-control/control.py via ngrok relay

---
+++ projects/lrm-vnc-macos-control/control.py
@@ -0,0 +1,22 @@
+import Quartz
+
+
+def move_mouse(x, y):
+    event = Quartz.CGEventCreateMouseEvent(
+        None, Quartz.kCGEventMouseMoved, (x, y), Quartz.kCGMouseButtonLeft
+    )
+    Quartz.CGEventPost(Quartz.kCGHIDEventTap, event)
+
+
+def click_mouse(x, y):
+    for event_type in [Quartz.kCGEventLeftMouseDown, Quartz.kCGEventLeftMouseUp]:
+        event = Quartz.CGEventCreateMouseEvent(
+            None, event_type, (x, y), Quartz.kCGMouseButtonLeft
+        )
+        Quartz.CGEventPost(Quartz.kCGHIDEventTap, event)
+
+
+def type_string(text):
+    for char in text:
+        event_down = Quartz.CGEventCreateKeyboardEvent(None, 0, True)
+        event_up = Quartz.CGEventCreateKeyboardEvent(None, 0_
