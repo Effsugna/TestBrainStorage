380b793d668fb118d44dc4020483d859d0a05627
Effsugna <effsugna@gmail.com>
2025-07-16 17:31:16 +0930

    Add projects/lrm-vnc-macos-control/test_run.py via ngrok relay

--- projects/lrm-vnc-macos-control/test_run.py
+++ projects/lrm-vnc-macos-control/test_run.py
@@ -1,17 +1,13 @@
 from screen import capture_screen
-from control import move_mouse, click_mouse
-import time
+from vision import analyze_screen
 
 if __name__ == "__main__":
     print("Capturing screen...")
     img = capture_screen()
-    img.save("test_screen.jpg")
-    print("Saved screenshot as test_screen.jpg")
 
-    print("Moving mouse to (100, 100)...")
-    move_mouse(100, 100)
-    time.sleep(1)
-    
-    print("Clicking mouse...")
-    click_mouse()
-    print("Done.")
+    print("Analyzing screen...")
+    objects = analyze_screen(img)
+
+    print("Detected objects:")
+    for obj in objects:
+        print(obj)
