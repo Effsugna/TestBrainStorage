0426eea3f37316bb5e512f46e24c5fd9adf7fd02
Effsugna <effsugna@gmail.com>
2025-07-18 14:30:11 +0930

    Small sleep sync to allow for typing to register from keycode after a mouse move and click

--- projects/lrm-vnc-macos-control/control.py
+++ projects/lrm-vnc-macos-control/control.py
@@ -1,4 +1,5 @@
 import Quartz
+import time
 
 
 def move_mouse(x, y):
@@ -20,4 +21,5 @@ def key_press(keycode):
     event_down = Quartz.CGEventCreateKeyboardEvent(None, keycode, True)
     event_up = Quartz.CGEventCreateKeyboardEvent(None, keycode, False)
     Quartz.CGEventPost(Quartz.kCGHIDEventTap, event_down)
+    time.sleep(0.01) # Add a small delay
     Quartz.CGEventPost(Quartz.kCGHIDEventTap, event_up)
